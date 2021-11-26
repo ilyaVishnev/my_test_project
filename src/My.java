3b3e4a4961b5f142476cfdaa311c069978a03e11
Ilia Vishnevskii <Ilia_Vishnevskii@epam.com>
2021-11-27 01:15:17 +0300

    five comment

--- src/My.java
+++ src/My.java
@@ -1,6 +1,6 @@
 public class My {
 
     public static void main(String[] args) {
-        System.out.println("Here is the first commit plus new merge");
+        System.out.println("Here is five");
     }
 }

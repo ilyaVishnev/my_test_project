98ad0e7ec69dab166927239611b0e55f181b8e6b
Ilia Vishnevskii <Ilia_Vishnevskii@epam.com>
2021-11-27 00:17:07 +0300

    new local br

--- src/My.java
+++ src/My.java
@@ -2,5 +2,8 @@ public class My {
 
     public static void main(String[] args) {
         System.out.println("Here is the first commit");
+        System.out.println("Here is the new local branch");
     }
+
+
 }

b43a4197705df517246c1b882cf2a3903775f867
Ilia Vishnevskii <Ilia_Vishnevskii@epam.com>
2021-11-27 00:42:03 +0300

    sec comment for new local

--- src/My.java
+++ src/My.java
@@ -1,7 +1,7 @@
 public class My {
 
     public static void main(String[] args) {
-        System.out.println("Here is the first commit");
+        System.out.println("Here is the new local first line");
         System.out.println("Here is the new local branch");
     }
 

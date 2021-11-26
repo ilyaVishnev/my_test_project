f0d22458d378837cc0211e8e60413aeec9779dec
Ilia Vishnevskii <Ilia_Vishnevskii@epam.com>
2021-11-27 00:21:14 +0300

    sec

--- src/My.java
+++ src/My.java
@@ -1,6 +1,6 @@
 public class My {
 
     public static void main(String[] args) {
-        System.out.println("Here is the first commit");
+        System.out.println("Here is the first commit plus new merge");
     }
 }

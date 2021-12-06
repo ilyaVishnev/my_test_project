03eee90c738c6ba8cd11f4ed08bd2c0be541811f
Ilia Vishnevskii <Ilia_Vishnevskii@epam.com>
2021-12-06 17:48:13 +0300

    for fetch sec

--- src/My.java
+++ src/My.java
@@ -3,6 +3,7 @@ public class My {
     public static void main(String[] args) {
         System.out.println("Here is the new local first line");
         System.out.println("Here is the new local branch");
+        System.out.println("cherry");
     }
 
 
